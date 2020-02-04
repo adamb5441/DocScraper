4352d1556639497fd01685181e38794f4dd8d290
adam <adam@pop-os.localdomain>
2020-02-03 21:30:19 -0500

    fixed 403 error issue and able to extract links

--- index.py
+++ index.py
@@ -1,25 +1,26 @@
-import urllib.request
+from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
 import pdfkit
 
 url = "https://libreboot.org/docs"
-
+url2 = "https://vuetifyjs.com/en/"
+url3 = "https://nodejs.org/en/docs/"
+url4 = "https://docs.microsoft.com/en-us/dotnet/csharp/"
 # print Single mage
 def printpage(url):
-    pdfkit.from_url('https://libreboot.org/docs/', 'out.pdf')
+    pdfkit.from_url(url, 'out.pdf')
 
 # will step threw and print everything with route example: https://libreboot.org/docs/
-def printContent():
+def printContent(url):
     # while True:
-    #     page = requests.get(url)
-    #     soup = BeautifulSoup(page.content, 'html.parser')
-    #     print(soup)
-    #     newLink = soup.find_all('a', href=True)[1]['href']
-         print("content")
+    req = Request(url, headers={'User-Agent': 'chrome/79'})
+    html = urlopen(req).read()
+    soup = BeautifulSoup(html, 'html.parser')
+    links = soup.find_all('a', href=True)
+    print(links)
 
 #this will prompt the user to select th content they want
 def custom():
     print("custom")
 
-printpage(url)
-print(complete)
+printContent(url4)
